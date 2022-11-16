6a7b053231d12c4eff0cb92b9d9c556a9ee3b063
Fernando Garcia <fernandgarcia97@hotmail.com>
2022-11-16 12:51:23 -0500

    step-19

--- test/auth_test.dart
+++ test/auth_test.dart
@@ -88,7 +88,8 @@ class MockAuthProvider implements AuthProvider {
     if (!isInitialized) throw NotInitializedException();
     if (email == 'foo@bar.com') throw UserNotFoundAuthException();
     if (password == 'foobar') throw WrongPasswordAuthException();
-    const user = AuthUser(isEmailVerified: false, email: 'foo@bar.com');
+    const user =
+        AuthUser(id: '1', isEmailVerified: false, email: 'foo@bar.com');
     _user = user;
     return Future.value(user);
   }
@@ -106,7 +107,8 @@ class MockAuthProvider implements AuthProvider {
     if (!isInitialized) throw NotInitializedException();
     final user = _user;
     if (user == null) throw UserNotFoundAuthException();
-    const newUser = AuthUser(isEmailVerified: true, email: 'foo@bar.com');
+    const newUser =
+        AuthUser(id: '1', isEmailVerified: true, email: 'foo@bar.com');
     _user = newUser;
   }
 }
